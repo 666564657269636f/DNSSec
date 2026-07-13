b3b3ded062602c5b5d77f6c137841dc1f39db888
666564657269636f <fede.c.lucio@gmail.com>
2026-07-13 15:08:14 +0200

    refactor: improve template loading

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 from pathlib import Path
-from typing import Any
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
 from yaml import safe_load
 
 
-CONFIG_PATH: Path = Path("config/lab.yml")
-DOCKER_COMPOSE_TEMPLATE_PATH: Path = Path("templates/docker-compose.yaml.j2")
-OUTPUT_PATH: Path = Path("output/docker-compose.yml")
+CONFIG_PATH: Path = Path('config')
+MODELS_PATH: Path = Path('models')
+OUTPUT_PATH: Path = Path('output')
+TEMPLATES_PATH: Path = Path('templates')
+
+DOCKER_COMPOSE_PATH: Path = OUTPUT_PATH / 'docker-compose.yml'
+DOCKER_COMPOSE_TEMPLATE_PATH: Path = TEMPLATES_PATH / 'docker-compose.yml.j2'
 
 
-def load_config(path: Path) -> dict[str, Any]:
+def load_config(path: Path) -> dict[str, list[dict[str, str]]]:
     with path.open("r", encoding="utf-8") as file:
-        data: dict[str, Any] = safe_load(file)
+        data: dict[str, list[dict[str, str]]] = safe_load(file)
 
     return data
 
 
-def generate_compose(
-    config: dict[str, Any],
-    template_path: Path,
-    output_path: Path,
-) -> None:
+def generate_compose(config: dict[str, list[dict[str, str]]], template_path: Path, output_path: Path) -> None:
+    parent: str = str(template_path.parent)
+    template_file = str(template_path.name)
 
-    template_directory: Path = template_path.parent
-    template_name: str = template_path.name
+    env: Environment = Environment(loader=FileSystemLoader(searchpath=parent))
+    template: Template = env.get_template(name=template_file)
 
-    environment: Environment = Environment(
-        loader=FileSystemLoader(str(template_directory))
-    )
+    compose_content: str = template.render(servers=config['servers'])
 
-    template = environment.get_template(template_name)
-
-    compose_content: str = template.render(
-        servers=config["servers"]
-    )
-
-    output_path.parent.mkdir(
-        parents=True,
-        exist_ok=True
-    )
-
-    output_path.write_text(
-        compose_content,
-        encoding="utf-8"
-    )
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    output_path.write_text(data=compose_content, encoding="utf-8")
 
 
 def main() -> None:
-    config: dict[str, Any] = load_config(CONFIG_PATH)
+    config: dict[str, list[dict[str, str]]] = load_config(path=CONFIG_PATH / 'lab.yml')
 
-    generate_compose(
-        config,
-        DOCKER_COMPOSE_TEMPLATE_PATH,
-        OUTPUT_PATH,
-    )
+    generate_compose(config=config, template_path=DOCKER_COMPOSE_TEMPLATE_PATH, output_path=DOCKER_COMPOSE_PATH)
 
 
 if __name__ == "__main__":
