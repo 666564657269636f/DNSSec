1abba9d969aa578fd806e88b0a709de74c4dbd1e
666564657269636f <fede.c.lucio@gmail.com>
2026-08-31 00:01:07 +0200

    feat(dns-lab): add Bind config generator

--- models/BindConfigGenerator.py
+++ models/BindConfigGenerator.py
@@ -6,40 +6,53 @@ from models.JinjaRender import JinjaRender
 
 class BindConfigGenerator:
 
-    TEMPLATE_BIND_PATH: Path = Path('templates/bind')
-
-
-    def __init__(self, server: Server):
+    def __init__(self, server: Server, input_dir: Path, output_dir: Path):
         self.server: Server = server
-        self.output_path: Path = Path(f'output/config/{ self.server.name }/')
+        self.input_dir: Path = input_dir
+        self.output_dir: Path = output_dir
 
 
-    def generate_config(self) -> None:
-        self._write_named_conf()
-        self._write_named_conf_options()
-        self._write_write_named_conf_local()
-        self._write_db_zone()
+    def generate(self) -> None:
+        self._db_zone()
+        self._named_conf()
+        self._named_conf_local()
+        self._named_conf_options()
 
 
-    def _write_db_zone(self) -> None:
-        template_path: Path = self.TEMPLATE_BIND_PATH / 'db.zone.j2'
-        output_path: Path = self.output_path / f'db.{ self.server.name }'
-        JinjaRender.render(template_path=template_path, output_path=output_path, name=self.server.name, ip=self.server.ip)
+    def _db_zone(self) -> None:
+        template_path: Path = self.input_dir / 'db.zone.j2'
+        output_path: Path = self.output_dir / f'db.{ self.server.name }'
+        JinjaRender.render(
+            template_path = template_path, 
+            output_path = output_path, 
+            name = self.server.name, 
+            ip = self.server.ip
+        )
 
 
-    def _write_named_conf(self) -> None:
-        template_path: Path = self.TEMPLATE_BIND_PATH / 'named.conf.j2'
-        output_path: Path = self.output_path / 'named.conf'
-        JinjaRender.render(template_path=template_path, output_path=output_path, context=None)
+    def _named_conf(self) -> None:
+        template_path: Path = self.input_dir / 'named.conf.j2'
+        output_path: Path = self.output_dir / 'named.conf'
+        JinjaRender.render(
+            template_path = template_path, 
+            output_path = output_path, 
+        )
 
 
-    def _write_write_named_conf_local(self): 
-        template_path: Path = self.TEMPLATE_BIND_PATH / 'named.conf.local.j2'
-        output_path: Path = self.output_path / 'named.conf.local'
-        JinjaRender.render(template_path=template_path, output_path=output_path, name=self.server.name)
+    def _named_conf_local(self): 
+        template_path: Path = self.input_dir / 'named.conf.local.j2'
+        output_path: Path = self.output_dir / 'named.conf.local'
+        JinjaRender.render(
+            template_path = template_path, 
+            output_path = output_path, 
+            name = self.server.name
+        )
 
 
-    def _write_named_conf_options(self):
-        template_path: Path = self.TEMPLATE_BIND_PATH / 'named.conf.options.j2'
-        output_path: Path = self.output_path / 'named.conf.options'
-        JinjaRender.render(template_path=template_path, output_path=output_path, context=None)
+    def _named_conf_options(self):
+        template_path: Path = self.input_dir / 'named.conf.options.j2'
+        output_path: Path = self.output_dir / 'named.conf.options'
+        JinjaRender.render(
+            template_path = template_path, 
+            output_path = output_path
+        )
